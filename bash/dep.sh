0f08b96fce192d7f09f39b76e56eb08e05648611
Protik Das <protik.das@kla-tencor.com>
2020-05-21 10:45:04 -0700

    add usage

--- bash/dep.sh
+++ bash/dep.sh
@@ -1,4 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+
+display_usage() { 
+	echo "This script can be used to find python packages that depends on a package." 
+	echo -e "\nUsage:\ndep.sh <package name> \n" 
+}
+
+if [  $# -le 1 ];then 
+    display_usage
+    exit 1
+fi 
 
 REQUIRES=$1
 
